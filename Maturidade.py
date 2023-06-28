70630afb6a031e1958abdd5ee44ce3e45f144283
deveucatur <117214850+deveucatur@users.noreply.github.com>
2023-06-28 10:30:14 -0400

    Update Complexidade.py

--- Maturidade.py
+++ Maturidade.py
@@ -1,81 +1,148 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
+import openpyxl
+from PIL import Image
 
-st.set_page_config(page_title="Maturidade de Processos")
+st.set_page_config(page_title="Complexidade de Cargo", page_icon=Image.open('imagens/logoeucatur.png'))
+
 
 col1, col2, col3 = st.columns(3)
 
 with col2:
     st.image('imagens/logoeucatur.png')
 
-def media(lista):
-    cont_numbers = 0
-    soma = 0
-    for a in lista:
-        cont_numbers += 1
-        soma += int(a)
+df = pd.read_excel("metricas.xlsx")
+listDados = df.values.tolist()
 
-    media_final = soma/cont_numbers
+def obter_nome_cargo(nivel, grau, tipo_carreira):
+    tabela = {
+        1: {
+            1: {
+                "ESPECIALIZAÇÃO": "Jovem Aprendiz/Estagiário",
+                "GESTÃO": "Jovem Aprendiz/Estagiário",
+                "LIDERANÇA TÉCNICA": None
+            },
+            2: {
+                "ESPECIALIZAÇÃO": "Auxiliares",
+                "GESTÃO": "Auxiliares",
+                "LIDERANÇA TÉCNICA": "Auxiliares"
+            }
+        },
+        2: {
+            3: {
+                "ESPECIALIZAÇÃO": "Assistentes",
+                "GESTÃO": "Assistentes",
+                "LIDERANÇA TÉCNICA": "Assistentes"
+            },
+            4: {
+                "ESPECIALIZAÇÃO": "Técnicos",
+                "GESTÃO": "Técnicos",
+                "LIDERANÇA TÉCNICA": "Técnicos"
+            }
+        },
+        3: {
+            5: {
+                "ESPECIALIZAÇÃO": "Analistas/Assessores",
+                "GESTÃO": "Coordenadores",
+                "LIDERANÇA TÉCNICA": "Encarregados Técnicos"
+            },
+            6: {
+                "ESPECIALIZAÇÃO": "Especialista Nível Superior",
+                "GESTÃO": "Gerentes",
+                "LIDERANÇA TÉCNICA": None
+            }
+        },
+        4: {
+            7: {
+                "ESPECIALIZAÇÃO": "Consultores Internos",
+                "GESTÃO": "Diretoria",
+                "LIDERANÇA TÉCNICA": None
+            },
+            8: {
+                "ESPECIALIZAÇÃO": None,
+                "GESTÃO": "Presidência",
+                "LIDERANÇA TÉCNICA": None
+            }
+        }
+    }
 
-    return media_final
+    try:
+        return tabela[nivel][grau][tipo_carreira]
+    except KeyError:
+        return None
 
-def indexmaior(lista):
-    cont = 0
-    id = 0
-    for i in lista:
-      cont +=1
-      if i == 3:
-        id = cont
 
-    return id
+def classifc_Nivel(soma):
+    if soma < 800:
+        return 1
+    elif soma >= 800 and soma < 1600:
+        return 2
+    elif soma >= 1600 and soma < 3200:
+        return 3
+    elif soma >= 3200:
+        return 4
 
-def plotarRadar(data, categories, Nomes):
-    fig = plt.figure(figsize=(6, 6))
-    ax = fig.add_subplot(111, polar=True)
-    # Define o limite dos eixos do gráfico
-    ax.set_ylim(1, 4)
-    # Define os rótulos para cada ponto dos dados
-    ax.set_xticklabels(categories)
-    # Cria uma lista de ângulos para cada ponto de dados
-    angles = np.linspace(0, 2 * np.pi, len(categories), endpoint=False)
-    # Adiciona os dados ao gráfico, especificando as posições de cada ponto
-    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', "purple"]
-    for i in range(len(data)):
-        # ax.plot(angles, data[i], linewidth=2,  marker='o')
-        ax.fill(angles, data[i], alpha=0.4, lw=3, ec=colors[i], fc=colors[i])
-    # Define a posição dos rótulos
-    ax.set_thetagrids(angles * 180 / np.pi, categories)
-    ax.grid(True)
-    ax.legend(Nomes, loc="upper center", bbox_to_anchor=(0.5, -0.125))
-    return (fig)
 
-col1, col2, col3 = st.columns((1,6,1))
-with col2:
-    st.title('Maturidade de Processos')
-
-metricas = pd.read_excel("Dados.xlsx")
-listMetric = metricas.values.tolist()
-
-classes = list(set([x[0] for x in listMetric]))
-
-optAval = ["1 - Falso",
-           "2 - Pouco Verdadeiro",
-           "3 - Amplamente Verdadeiro"]
-
+def classif_Grau(soma):
+    if soma < 600:
+        return 1
+    elif soma >= 600 and soma < 800:
+        return 2
+    elif soma >= 800 and soma < 1200:
+        return 3
+    elif soma >= 1200 and soma < 1600:
+        return 4
+    elif soma >= 1600 and soma < 2400:
+        return 5
+    elif soma >= 2400 and soma < 3200:
+        return 6
+    elif soma >= 3200:
+        return 7
 
 
     
-with st.form("my_form"):
+st.title("Complexidade dos cargos")
+col1, col2, col3 = st.columns((0.5, 1.7, 1))
+with col1:
+    matricula = st.text_input("Nº Matrícula")
+
+with col2:
+    name = st.text_input("Nome Gestor de Carreira")
+
+with col3:
+    funcao = st.selectbox('Função', ["Líder de Processos", "Dono de processo", "Gestor de Processos",
+                                             "Executor de Processos"])
+
+col1, col2 = st.columns(2)
+with col1:
+    Unidade = st.selectbox("Unidade de negócio", ["CEEM Ariquemes",
+                                                          "CEEM Boa Vista - Manaus",
+                                                          "CEEM Cacoal",
+                                                          "CEEM Campinas",
+                                                          "CEEM Campo Grande",
+                                                          "CEEM Cascavel",
+                                                          "CEEM Cuiaba",
+                                                          "CEEM Curitiba",
+                                                          "CEEM Goiânia",
+                                                          "CEEM Jí-Parana",
+                                                          "CEEM Porto Alegre",
+                                                          "CEEM Porto Velho",
+                                                          "CEEM Pres. Prudente",
+                                                          "CEEM Rio Branco",
+                                                          "CEEM Rondonópolis",
+                                                          "CEEM São Paulo",
+                                                          "CEEM Vilhena",
+                                                          "Corporativo Cascavel",
+                                                          "Corporativo Jí-Parana"])
+with col2:
     macroprocesso = st.selectbox('Macroprocesso', ["Relacionamento com Cliente - Pessoas",
                                                            "Relacionamento com Cliente - Cargas",
                                                            "Administrar",
                                                            "Operar",
                                                            "Formulação Estratégica"])
 
-    processo = st.selectbox('Processo', ["Prospectar - Pessoas",
+if macroprocesso != "Formulação Estratégica" and funcao != "Líder de Processos":
+    processo = st.multiselect('Processo', ["Prospectar - Pessoas",
                                                    "Vender - Pessoas",
                                                    "Transportar - Pessoas",
                                                    "Prospectar - Cargas",
@@ -87,107 +154,71 @@ with st.form("my_form"):
                                                    "Administrar e Previnir o Risco Contencioso",
                                                    "Operar Ativos",
                                                    "Operar a Cadeia de Suprimentos"])
-    
-    procedimento = st.text_input('Procedimento')
-    lista_classes = []
-    for a in range(len(classes)):
-        st.write("---")
-        col1, col2, col3 = st.columns(3)
+else:
+    processo = ""
+
+st.write("---")
+
+tipo_carr = st.selectbox('Tipo de Carreira', ['ESPECIALIZAÇÃO', 'GESTÃO', 'LIDERANÇA TÉCNICA'])
+
+escopofunç = st.text_area("Escopo da função")
+
+
+fatores = set([x[0] for x in listDados])
+print(fatores)
+
+lista = []
+soma = 0
+for i in fatores:
+    st.subheader(f'{i}')
+
+    genre = st.radio(
+        "Selecione uma opção abaixo",
+        [x[2] for x in listDados if x[0] == i])
+
+    grau = [x[1] for x in listDados if x[2] == genre]
+
+    peso_complix = 25 * (2**(grau[0] - 1))
+
+    soma += peso_complix
+
+    lista.append([i, genre, grau, peso_complix])
+
+
+grau_class = classif_Grau(soma)
+nivel_class = classifc_Nivel(soma)
+st.write("---")
+
+
+
+if st.button("Gerar Relatório"):
+    st.write("---")
+
+    st.subheader("Relatório de Complexidade do cargo")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.text_input(f'Cargo', obter_nome_cargo(nivel_class, grau_class, tipo_carr))
+    with col2:
+        st.text_input(f'Nivel', nivel_class)
+    with col3:
+        st.text_input(f'Grau', grau_class)
+
+
+    st.write("---")
+
+    st.text_area("Escopo", escopofunç)
+
+    st.write("---")
+    st.subheader("Resumo dos Fatores")
+
+    for a in lista:
+        col1, col2 = st.columns((1, 2))
+
+        with col1:
+            st.warning(f'{a[0]}')
+
         with col2:
-            st.title(classes[a])
-        
-        fatorList = set([x[1] for x in listMetric if x[0] == classes[a]])
-        
-        lista_fatores = []
-
-        for fator in fatorList:
-            st.subheader(f'{fator}')
-            valor_fatores = []
-            for alter in [x[2] for x in listMetric if x[0] == classes[a] and x[1] == fator]:
-                nota = int([st.select_slider(alter, options=optAval, value=optAval[1])][0][0:1])
-
-                #st.write(nota)
-
-                valor_fatores.append(nota)
-
-            lista_fatores.append([fator, indexmaior(valor_fatores)])
-        lista_classes.append([classes[a], lista_fatores])
-    submitted = st.form_submit_button("ENVIAR")
-    if submitted:
-        #st.write(lista_classes)
-        st.write('---')
-        col1, col2, col3 = st.columns((1.5,8,1))
-        with col2:
-            st.title('Apresentação das Classes')
-        
-        st.text(' ')
-
-        
-        #st.write(lista_classes)
-        for a in range(len(classes)):
-            #with col2:
-            st.subheader(f'Maturidade dos Fatores ({classes[a]})')
-            st.text(' ')
-            #st.write(f'{lista_classes[a][0]}')
-            fatoresLista = lista_classes[a][1]
-            for fatores in fatoresLista:
-                col1, col2 = st.columns((4,2))
-                with col1:
-                    st.warning(f'{fatores[0]}')
-                with col2:
-                    st.info(fatores[1])
-    
-            media_fatores = media([fatoresLista[x][1] for x in range(len(fatoresLista))]) 
-            
-            col1, col2 = st.columns((4,2))
-            with col1:
-                st.info(f'Maturidade da Classe ({classes[a]})')
-            with col2:
-                st.info(round(media_fatores))
-
-st.text(' ')
-st.text(' ')
-
-st.title('Gráfico Radar')
-    
-notas_list = [[x[1][y][1]for y in range(len(x[1]))] for x in lista_classes]
-nomes_list = [[x[1][y][0]for y in range(len(x[1]))] for x in lista_classes]
-
-fatorLista = [lista_classes[x][1] for x in range(len(classes))]
-
-media = []
-for a in fatorLista:
-    numbers = 0
-    cont = 0
-    for b in a:
-        cont += 1
-        numbers += b[1]
-    media.append(numbers / cont)
-
-nomes = []
-notas = []
-
-for lista in nomes_list:
-    for nome in lista:
-        nomes.append(nome)
-
-for a in notas_list:
-    for b in a:
-        notas.append(b)
-    
-col1, col2, = st.columns(2)
-        
-with col1:
-    st.subheader('Fatores')
-    st.pyplot(plotarRadar([notas], nomes, processo))
-with col2:
-    st.subheader('Classes')
-    st.pyplot(plotarRadar([media], classes, processo))
-
-                
-
-
-
- 
-        
+            st.info(f'{a[1]}')
 
